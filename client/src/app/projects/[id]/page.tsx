c4e178afa3557b5018f2db91db5b7bcbd8b80fa5
dhruvgupta114 <dhruv.work114@gmail.com>
2024-11-15 17:25:41 +0530

    all changes changed + added authentication and authorization

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,44 +1,13 @@
-"use client";
-
-import React, { useState } from "react";
-import ProjectHeader from "@/app/projects/ProjectHeader";
-import Board from "../BoardView";
-import List from "../ListView";
-import Timeline from "../TimelineView";
-import Table from "../TableView";
-import ModalNewTask from "@/components/ModalNewTask";
+import Project from "./Project";
 
 type Props = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
-const Project = ({ params }: Props) => {
-  const { id } = params;
-  const [activeTab, setActiveTab] = useState("Board");
-  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+const ProjectPage = async ({ params }: Props) => {
+  const { id } = await params; // Resolve the Promise here
 
-  return (
-    <div>
-      <ModalNewTask
-        isOpen={isModalNewTaskOpen}
-        onClose={() => setIsModalNewTaskOpen(false)}
-        id={id}
-      />
-      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-      {activeTab === "Board" && (
-        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
-      {activeTab === "List" && (
-        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
-      {activeTab === "Timeline" && (
-        <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
-      {activeTab === "Table" && (
-        <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
-      )}
-    </div>
-  );
+  return <Project id={id} />;
 };
 
-export default Project;
+export default ProjectPage;
