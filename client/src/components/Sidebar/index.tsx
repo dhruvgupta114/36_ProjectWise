849d464c5e0267d45d726f5d315db31dd3770fd5
dhruvgupta114 <dhruv.work114@gmail.com>
2024-11-15 15:29:39 +0530

    images added with link

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -54,7 +54,7 @@ const Sidebar = () => {
                 </div>
                 {/* TEAM */}
                 <div className='flex items-center gap-5 border-y-[1.5px] border-gray-600 px-8 py-4  dark:border-gray-700'>
-                    <Image src="/logo.png" alt='Logo' width={40} height={40} />
+                    <Image src="https://pm-s3-imag.s3.us-east-1.amazonaws.com/logo.png" alt='Logo' width={40} height={40} />
                     <div>
                         <h3 className='text-md font-bold tracking-wide dark:text-gray-200'>
                             EDROH TEAM
